36d03b91daeebdaa581997350bba75d5bfabcbc2
cskumaran19 <developerkumarancs@gmail.com>
2025-10-21 20:30:49 +0530

    Add cicd

--- models/train_model.py
+++ models/train_model.py
@@ -3,7 +3,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 import joblib
 
-df = pd.read_pickle('../data/processed/cleaned_tax_data.pkl')
+df = pd.read_pickle('data/processed/cleaned_tax_data.pkl')
 X = df[['year', 'income_bracket']]
 y = df['tax_amount']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
